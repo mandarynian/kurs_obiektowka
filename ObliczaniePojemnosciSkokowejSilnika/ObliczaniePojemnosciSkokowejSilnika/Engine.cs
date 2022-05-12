b7aba04b7793909411323022055732a52dc94ede
Krystian <cris.tokarczyk@gmail.com>
2022-05-12 20:13:30 +0200

    feat: add cylinder class

---
+++ ObliczaniePojemnosciSkokowejSilnika/ObliczaniePojemnosciSkokowejSilnika/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObliczaniePojemnosciSkokowejSilnika.Engines
+{
+    public class Engine
+    {
+        public string Name { get; set; }
+
+        public List<Cylinder> Cylinders { get; set; }
+
+
+        public Engine(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
