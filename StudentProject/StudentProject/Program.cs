74568f5853e75d20d21794acf879a3a60730f726
Krystian <cris.tokarczyk@gmail.com>
2022-07-25 22:44:17 +0200

    feat: add new project to repository

---
+++ StudentProject/StudentProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudentProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student("Michał", "Tokarczyk",1);
+
+            Console.WriteLine(student.ShowStudent());
+        }
+    }
+}
