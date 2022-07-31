02d132dd4272767f5cb85f92bc7345371ca65206
Krystian <cris.tokarczyk@gmail.com>
2022-07-31 22:55:59 +0200

    new project - algorytmy

---
+++ AlgorytmySortowania/AlgorytmySortowania/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlgorytmySortowania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.WriteLine("Podaj liczbę w zbiorze do posrotowania: ");
+            int liczba = Convert.ToInt32(Console.ReadLine());
+            Buble buble = new Buble(liczba);
+        }
+
+
+    }
+}
